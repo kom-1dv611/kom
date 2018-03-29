2d4f6d9834207deb7512a8465c0f6bc56cc1654e
Lukas Nilsson <ln222pd@student.lnu.se>
2018-03-29 22:29:06 +0200

    Cleaned up clock code.

--- public/client/showTime.js
+++ public/client/showTime.js
@@ -1,3 +1,7 @@
+var clock;
+var animation = "animated fadeInDown";
+var animationTime = "0.7s";
+
 function getLength(number) {
     return number.toString().length
 }
@@ -9,53 +13,50 @@ function ensureFormat(time) {
     return time;
 }
 
-function getTime() {
-    let now = new Date()
-    let toReturn = {hours: now.getHours(), minutes: now.getMinutes(), seconds: now.getSeconds()};
-
-    let keys = Object.keys(toReturn);
-    let values = Object.values(toReturn);
-
-    keys.forEach(function(key, i) {
-        toReturn[key] = ensureFormat(values[i]);
-    });
-
-    return toReturn
-}
-
-let header;
-
-$(document).ready(function() {
-    header = $("#clock");
-    displayTime()
-});
-
 function makeSpans(parent) {
     for(let i = 0; i < 8; i++) {
-        parent.append(`<span data-wow-duration="0.7s"></span>`);
+        parent.append(`<span></span>`);
     }
 }
 
-function updateSpan(child) {
-    if(child.text() != toDisplay[i]) {
-        child.addClass("animated fadeInDown");
-        setTimeout(removeAnimation, 900, child);
-        child.text(toDisplay[i]);
-    }
+function Clock(parent) {
+    this.parent = parent;
+
+    $(parent).attr("data-wow-duration", animationTime);
+    makeSpans(parent);
+
+    setInterval(this.displayTime.bind(this), 1000); //updates every second
+    this.displayTime(); //starts clock
 }
 
-function displayTime() {
-    let time = getTime()
-    let toDisplay = `${time.hours}:${time.minutes}:${time.seconds}`;
+Clock.prototype.displayTime = function() {
+    console.log(this);
+    let time = this.getTime()
+    let toDisplay = `${this.hours}:${this.minutes}:${this.seconds}`;
     let child;
     for(let i = 0; i < toDisplay.length; i++) {
-        child = $(parent.children()[i]);
-        updateSpan(child)
+        child = $(this.parent.children()[i]);
+        this.updateSpan(child, toDisplay[i])
     }
 }
 
-function removeAnimation(child) {
-    $(child).removeClass("animated fadeInDown");
+Clock.prototype.getTime = function() {
+    let now = new Date();
+    this.hours = ensureFormat(now.getHours());
+    this.minutes = ensureFormat(now.getMinutes());
+    this.seconds = ensureFormat(now.getSeconds());
 }
 
-setInterval(displayTime, 1000);
+Clock.prototype.updateSpan = function(child, update) {
+    if(child.text() != update) {
+        child.addClass(animation);
+        setTimeout(this.removeAnimation, 900, child);
+        child.text(update);
+    }
+}
+
+Clock.prototype.removeAnimation = function(child) {
+    $(child).removeClass(animation);
+}
+
+clock = new Clock($("#clock"));
