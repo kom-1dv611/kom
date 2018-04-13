09ad93e2f03993c7c0335e74cc2b307ff7ad10c5
Jonas Erixon <x.jonaz@gmail.com>
2018-04-13 17:44:50 +0200

    scraping all group rooms in kalmar

--- libs/scraper.js
+++ libs/scraper.js
@@ -1,62 +1,70 @@
 'use strict';
 
-const cheerio = require('cheerio');
-const rp = require('request-promise');
-
 const puppeteer = require('puppeteer');
 
-
-const LNU_ROOM_URL = 'https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&s=alphabeticallyasc';
+const LNU_KALMAR_ROOMS = [
+    'https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&s=alphabeticallyasc',
+    'https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&p=1&s=alphabeticallyasc',
+    'https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&p=2&s=alphabeticallyasc',
+    'https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&p=3&s=alphabeticallyasc'
+];
 
 module.exports = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    let rooms = [];
     
-    await page.goto('https://lnu.se/sok/?q=grupprum&main=Karta&locationcity=Kalmar&p=2&s=alphabeticallyasc');
-
-    try {
-    
-        const finder = await page.evaluate(() => {
-            let lists = document.querySelector('.content-list > ul');
-            let roomList = [];
-
-            for (let i = 0; i < lists.childElementCount; i++) {
-                roomList.push(lists.children[i].textContent)
-            }
-
-            return roomList;
-        });
-
-        let rooms = [];
-
-        for (let i = 0; i < finder.length; i++) {
-            let room = finder[i].replace(/\t/g, '');
-            room = room.replace(/(\r\t\n|\n|\r\t)/gm,' ');
-            room = room.split(' ');
-
-            let roomObject = {};
-
-            roomObject.name = room[2];
-            roomObject.city = room[3];
-            roomObject.type = room[6];
-            roomObject.floor_type = room[7];
-            roomObject.floor = room[8];
-            roomObject.location = room[9];
-
-            rooms.push(roomObject);
-
-            console.log(room)
-        }
-
+    for (let i = 0; i < LNU_KALMAR_ROOMS.length; i++) {
         
+        await page.goto(LNU_KALMAR_ROOMS[i]);
 
-        browser.close();
-
-        return rooms;
-
-    } catch (error) {
-        console.log(error)
+        try {
+            const finder = await page.evaluate(() => {
+                let lists = document.querySelector('.content-list > ul');
+                let roomList = [];
+    
+                for (let i = 0; i < lists.childElementCount; i++) {
+                    roomList.push(lists.children[i].textContent)
+                }
+    
+                return roomList;
+            });
+    
+            
+            for (let i = 0; i < finder.length; i++) {
+                let room = finder[i].replace(/\t/g, '');
+                room = room.replace(/(\r\t\n|\n|\r\t)/gm,' ');
+                room = room.split(' ');
+    
+                let roomObject = {};
+    
+                if (room[2] == 'Grupprum') {
+                    roomObject.name = room[3];
+                    roomObject.city = room[4];
+                    roomObject.type = room[7];
+                    roomObject.floor_type = room[8];
+                    roomObject.floor = room[9];
+                    roomObject.location = room[10];
+                } else {
+                    roomObject.name = room[2];
+                    roomObject.city = room[3];
+                    roomObject.type = room[6];
+                    roomObject.floor_type = room[7];
+                    roomObject.floor = room[8];
+                    roomObject.location = room[9];
+                }
+                
+                rooms.push(roomObject);
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
+
+    browser.close();
+
+    return rooms;
 };
 
 
