875b9c3650880a5f126d52914245eeb143ec90ca
Alva Fandrey <af222ug@student.lnu.se>
2018-04-16 14:19:51 +0200

    indikera tillgänglighet på specificRoom

--- routes/routes.js
+++ routes/routes.js
@@ -1,43 +1,56 @@
-'use strict';     
+'use strict';
 
 const Scraper = require('../libs/scraper');
 
 const timeEditApi = require('timeeditApi');
 const timeEdit = timeEditApi(
-    'https://se.timeedit.net/web/lnu/db1/schema1/', 4                                              
+    'https://se.timeedit.net/web/lnu/db1/schema1/', 4
 );
 
 let router = require("express").Router();
 let roomID;
-    
-module.exports = function(RoomModel) {
-    
+let moment = require('moment');
 
-router.route('/')
-        .get(async function(req, res) {
+module.exports = function (RoomModel) {
+
+
+    router.route('/')
+        .get(async function (req, res) {
 
             let rooms = await Scraper();
-        
+
             let size = Math.ceil(rooms.length / 3);
             let rows = [];
-            for(let i = 0; i < size; i++) {
+            for (let i = 0; i < size; i++) {
                 rows.push({})
                 rows[i].cols = [];
-                for(let j = i * 3; j < (i * 3) + 3; j++) {
-                    if(rooms[j] != undefined) {
+                for (let j = i * 3; j < (i * 3) + 3; j++) {
+                    if (rooms[j] != undefined) {
                         rows[i].cols.push(rooms[j]);
                     }
                 }
             }
-    
-            res.render("index", {rows: rows});
+
+            res.render("index", { rows: rows });
         })
 
     router.route('/:id')
-        .get(function(req, res) {
+        .get(function (req, res) {
             roomID = req.params.id
-
-            res.render("room", {room: req.params.id});
+            let available;
+            timeEdit.getTodaysSchedule(req.params.id)
+                .then((roomSchedule) => {
+                    moment.locale('sv');
+                    if (roomSchedule === null) {
+                        available = true
+                    } else if (moment().format('LT') > roomSchedule[0].time.startTime) {
+                        available = false
+                    }
+                }).then(() => {
+                    res.render("room", { room: req.params.id, available: available });
+                }).catch((er) => {
+                    console.log(er);
+                });
         })
         .post(function (req, res) {
             if (req.body.username === undefined) {
@@ -45,13 +58,13 @@ router.route('/')
                 req.session.flash = {
                     type: 'fail',
                     message: 'You must write a username'
-                }; 
+                };
             } else {
                 let data = {
                     username: req.body.username,
                     time: req.body.time
                 }
-    
+
                 let bookRoom = new RoomModel(data)
                 bookRoom.save((err) => {
                     console.log('saved')
@@ -63,40 +76,40 @@ router.route('/')
     router.route('/:roomID/schedule/today')
         .get(function (req, res) {
             timeEdit.getTodaysSchedule(req.params.roomID)
-            .then((roomSchedule) => {
-                let data = {
-                    startTime: roomSchedule[0].time.startTime,
-                    endTime: roomSchedule[0].time.endTime,
-                    bookingID: roomSchedule[0].bookingId,
-                    info: roomSchedule[0].columns[2]
-                }
-                console.log(data);
-                res.send(JSON.stringify(data, null, 2));
-            }).catch((er) => {
-                console.log(er);
-            });
-    });
+                .then((roomSchedule) => {
+                    let data = {
+                        startTime: roomSchedule[0].time.startTime,
+                        endTime: roomSchedule[0].time.endTime,
+                        bookingID: roomSchedule[0].bookingId,
+                        info: roomSchedule[0].columns[2]
+                    }
+                    console.log(data);
+                    res.send(JSON.stringify(data, null, 2));
+                }).catch((er) => {
+                    console.log(er);
+                });
+        });
 
     router.route('/room/:roomID/schedule/')
         .get(function (req, res) {
             // full schedule     
             timeEdit.getSchedule(req.params.roomID)
-            .then((schedule) => {
-                let array = [];
-                for(let i = 0; i < schedule.length; i++) {
-                    let data = {
-                        startTime: schedule[i].time.startTime,
-                        endTime: schedule[i].time.endTime,
-                        bookingID: schedule[i].bookingId,
-                        info: schedule[i].columns[2]
+                .then((schedule) => {
+                    let array = [];
+                    for (let i = 0; i < schedule.length; i++) {
+                        let data = {
+                            startTime: schedule[i].time.startTime,
+                            endTime: schedule[i].time.endTime,
+                            bookingID: schedule[i].bookingId,
+                            info: schedule[i].columns[2]
+                        }
+                        array.push(data);
                     }
-                    array.push(data);
-                }
-                res.send(JSON.stringify(array, null, 2));
-            }).catch((er) => {
-                console.log(er);
-            });
+                    res.send(JSON.stringify(array, null, 2));
+                }).catch((er) => {
+                    console.log(er);
+                });
         });
 
-        return router;
-    }
+    return router;
+}
