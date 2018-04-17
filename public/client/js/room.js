c44c34c14941a7d8625e734a65f39b548f9eaa7b
Lukas Nilsson <ln222pd@student.lnu.se>
2018-04-17 17:51:07 +0200

    Date and time input is now optional.

--- public/client/js/room.js
+++ public/client/js/room.js
@@ -1,19 +1,32 @@
 let lastClick;
 const socket = io();
 
-$(".preset").click(function() {
-    lastClick = this;
-});
-
 $(".submit").click(function() {
     let username = $("#username").val()
-    let time = $(lastClick).text();
+    //let time = $(lastClick).text();
 
-    socket.emit("newBooking", {username: username, time: time});
+    //socket.emit("newBooking", {username: username, time: time});
 });
 
-if($("#state").text() == "Available") {
-    $("body").attr('id', 'available');
-} else {
-    $("body").attr('id', 'unavailable');
+$("body").attr('id', $("#state").text() == "Available" ? 'available' : 'unavailable');
+
+function makeCol(parent) {
+    let col = $("<div>").prependTo("#bookingForm");
+    col.addClass("col-md-auto");;
+    return col;
 }
+
+$("#bookLater").click(function() {
+    let col, time, date;
+    $(this).parent().remove();
+
+    col = makeCol("#bookingForm");
+    time = $("<input>").appendTo(col);
+    time.addClass("form-control");
+    time.attr("type", "time")
+
+    col = makeCol("#bookingForm");
+    date = $("<input>").appendTo(col);
+    date.addClass("form-control");
+    date.attr("type", "date");
+});
