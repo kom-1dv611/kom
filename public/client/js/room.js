76c3061949002358607d4394915c1bb4ef04c823
Lukas Nilsson <ln222pd@student.lnu.se>
2018-04-17 11:22:35 +0200

    Added logo and text.

--- public/client/js/room.js
+++ public/client/js/room.js
@@ -2,18 +2,6 @@ let lastClick;
 const socket = io();
 
 $(".preset").click(function() {
-    let clicked = $(this);
-    let normal = "btn-dark";
-    let selected = "btn-primary";
-
-    $(clicked).removeClass(normal);
-    $(clicked).addClass(selected);
-
-    if(lastClick != undefined) {
-        $(lastClick).removeClass(selected);
-        $(lastClick).addClass(normal)
-    }
-
     lastClick = this;
 });
 
