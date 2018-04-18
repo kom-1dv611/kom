1f3d81c309a0ca80d32172484d8d4659d6db5a7a
Lukas Nilsson <ln222pd@student.lnu.se>
2018-04-18 17:02:44 +0200

    Added icons, added animations.

--- public/client/js/room.js
+++ public/client/js/room.js
@@ -12,21 +12,31 @@ $("body").attr('id', $("#state").text() == "Available" ? 'available' : 'unavaila
 
 function makeCol(parent) {
     let col = $("<div>").prependTo("#bookingForm");
-    col.addClass("col-md-auto");;
+    col.addClass("col-md-auto animated fadeInLeft");;
     return col;
 }
 
 $("#bookLater").click(function() {
-    let col, time, date;
+    let col, time, date, dateIcon, timeIcon;
     $(this).parent().remove();
 
     col = makeCol("#bookingForm");
+
+    dateIcon = $("<i>").appendTo(col);
+    dateIcon.addClass("fas fa-calendar-alt fa-2x");
+
     time = $("<input>").appendTo(col);
     time.addClass("form-control");
     time.attr("type", "time")
 
     col = makeCol("#bookingForm");
+
+    dateIcon = $("<i>").appendTo(col);
+    dateIcon.addClass("fas fa-clock fa-2x");
+
     date = $("<input>").appendTo(col);
     date.addClass("form-control");
     date.attr("type", "date");
+
+    $(".btn-group").children().addClass("animated fadeInLeft");
 });
